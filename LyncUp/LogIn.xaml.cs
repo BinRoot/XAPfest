79a9d78f0f78dae94fe415f7cc5f72bc659d0506
Nishant Shukla <nick722@gmail.com>
2012-06-17 18:02:40 -0700

    added missing methods in login.xaml.cs

--- LyncUp/LogIn.xaml.cs
+++ LyncUp/LogIn.xaml.cs
@@ -10,14 +10,73 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Live;
+using Microsoft.Live.Controls;
 
 namespace LyncUp
 {
     public partial class LogIn : PhoneApplicationPage
     {
+        private LiveConnectClient client;
+
         public LogIn()
         {
             InitializeComponent();
+        }
+
+        private void btnSignin_SessionChanged(object sender, LiveConnectSessionChangedEventArgs e)
+        {
+            if (e.Status == LiveConnectSessionStatus.Connected)
+            {
+                client = new LiveConnectClient(e.Session);
+                infoTextBlock.Text = "Signed in.";
+                client.GetCompleted += 
+                    new EventHandler<LiveOperationCompletedEventArgs>(OnGetCompleted);
+                client.GetAsync("me", null);
+            }
+            else
+            {
+                infoTextBlock.Text = "Not signed in.";
+                client = null;
+            }
+        }
+
+        void OnGetCompleted(object sender, LiveOperationCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                string firstName = "";
+                string lastName = "";
+                if (e.Result.ContainsKey("first_name") || 
+                    e.Result.ContainsKey("last_name"))
+                {
+                    if (e.Result.ContainsKey("first_name"))
+                    {
+                        if (e.Result["first_name"] != null)
+                        {
+                            firstName = e.Result["first_name"].ToString();
+                        }
+                    }
+                    if (e.Result.ContainsKey("last_name"))
+                    {
+                        if (e.Result["last_name"] != null)
+                        {
+                            lastName = e.Result["last_name"].ToString();
+                        }
+                    }
+                    infoTextBlock.Text =
+                        "Hello, " + firstName + lastName + "!";
+                }
+                else
+                {
+                    infoTextBlock.Text = "Hello, signed-in user!";
+                }
+            }
+            else 
+            {
+                infoTextBlock.Text = "Error calling API: " + 
+                    e.Error.ToString();
+            }
         }
     }
 }
